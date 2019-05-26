628d96fdb1708798ac03860b12203f683a6f21c4
Isidro Henoch <isidro.henoch@gmail.com>
2019-05-26 16:47:00 -0500

    Change the way the app starts, if the user is logged shows the home, the login is displayed otherwise

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:cinnamon/shared/constants.dart';
+import 'package:cinnamon/ui/home.dart';
 import 'package:cinnamon/ui/login.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
-  runApp(CinnamonApp());
-}
+  var preferences = await SharedPreferences.getInstance();
+  var isUserLogged = preferences.containsKey(Constants.ID_PREFERENCE);
 
-class CinnamonApp extends StatefulWidget {
-  @override
-  _CinnamonAppState createState() => _CinnamonAppState();
-}
-
-class _CinnamonAppState extends State<CinnamonApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cinnamon',
-      home: LoginPage(),
-    );
-  }
+  runApp(MaterialApp(
+    title: "Cinnamon",
+    home: isUserLogged ? HomePage() : LoginPage(),
+  ));
 }
