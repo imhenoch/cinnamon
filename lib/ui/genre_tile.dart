f5f0eadaf0ab5b1d6c5b47165057a19f7f2c7cbb
Isidro Henoch <isidro.henoch@gmail.com>
2019-05-28 17:58:26 -0500

    Greatly improves the widget that displays a [favorite] genre

--- lib/ui/genre_tile.dart
+++ lib/ui/genre_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cinnamon/models/genre.dart';
 import 'package:flutter/material.dart';
+import 'package:cinnamon/api/cinnema_api.dart';
 
 class GenreTile extends StatefulWidget {
   final LocalGenre localGenre;
@@ -11,16 +12,40 @@ class GenreTile extends StatefulWidget {
 }
 
 class _GenreTileState extends State<GenreTile> {
-  @override
-  void initState() {
-    super.initState();
+  _favorite(LocalGenre genre) {
+    if(genre.isFavorite) {
+      CinnemaApi.removeFavoriteGenre(genre.rawGenre);
+    } else {
+      CinnemaApi.saveFavoriteGenre(genre.rawGenre);
+    }
+    setState(() => widget.localGenre.isFavorite = !genre.isFavorite);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-        "${widget.localGenre.rawGenre.genre} ${widget.localGenre.isFavorite
-            ? "true"
-            : "false"}");
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 4.0),
+        child: Card(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+                      child: Text(
+                          widget.localGenre.rawGenre.genre,
+                          style: TextStyle(fontSize: 18)
+                      )
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(widget.localGenre.isFavorite
+                      ? Icons.favorite
+                      : Icons.favorite_border),
+                  onPressed: () => _favorite(widget.localGenre),
+                )
+              ],
+            )
+        )
+    );
   }
 }
