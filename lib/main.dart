11e4ff103dc26fdbe616819e55ba45d8ea12ec4f
Isidro Henoch <isidro.henoch@gmail.com>
2019-05-28 14:17:31 -0500

    Minor changes on the main file

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,23 @@ import 'package:cinnamon/ui/login.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'api/cinnema_api.dart';
+
 Future<void> main() async {
   var preferences = await SharedPreferences.getInstance();
   var isUserLogged = preferences.containsKey(Constants.ID_PREFERENCE);
 
   runApp(MaterialApp(
     title: "Cinnamon",
-    home: isUserLogged ? HomePage() : LoginPage(),
+    home: initialScreen(isUserLogged),
   ));
+}
+
+Widget initialScreen(bool isUserLogged) {
+  if (isUserLogged) {
+    CinnemaApi.loadToken();
+    return HomePage();
+  } else {
+    return LoginPage();
+  }
 }
