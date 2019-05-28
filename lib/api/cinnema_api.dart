6fd8e85ae88d19868f80aa6a5320bab1afd83a90
Isidro Henoch <isidro.henoch@gmail.com>
2019-05-28 14:16:53 -0500

    Refactors api class and adds to it a function to load the logged user information.

--- lib/api/cinnema_api.dart
+++ lib/api/cinnema_api.dart
@@ -7,9 +7,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class CinnemaApi {
   static final baseUrl = "http://192.168.1.66:8080";
+  static var _token = "";
   static final headers = {
     "Accept": "application/json",
-    "content-type": "application/json"
+    "content-type": "application/json",
+    "Authorization": "Bearer $_token"
   };
 
   static Future<User> login(String email, String password) async {
@@ -23,10 +25,24 @@ class CinnemaApi {
       preferences.setString(Constants.EMAIL_PREFERENCE, user.email);
       preferences.setInt(Constants.ID_PREFERENCE, user.id);
       preferences.setString(Constants.TOKEN_PREFERENCE, user.token);
+      _token = user.token;
 
       return user;
     } else {
       throw Exception('Failed to login');
     }
+  }
+
+  static void loadToken() async {
+    var preferences = await SharedPreferences.getInstance();
+    _token = preferences.getString(Constants.TOKEN_PREFERENCE);
+  }
+
+  static Future<User> getUser() async {
+    var preferences = await SharedPreferences.getInstance();
+    return User(
+        id: preferences.getInt(Constants.ID_PREFERENCE),
+        email: preferences.getString(Constants.EMAIL_PREFERENCE),
+        token: _token);
   }
 }
