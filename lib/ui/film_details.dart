8544362a6ba4784fccba81bcba48f96a79e18906
Isidro Henoch <isidro.henoch@gmail.com>
2019-06-06 01:14:51 -0500

    Changes the film details widget from stateless to stateful

--- lib/ui/film_details.dart
+++ lib/ui/film_details.dart
@@ -1,98 +1,84 @@
-import 'package:flutter/material.dart';
+import 'package:cinnamon/api/cinnema_api.dart';
 import 'package:cinnamon/models/film.dart';
+import 'package:flutter/material.dart';
 
-class FilmPage extends StatelessWidget {
+class FilmPage extends StatefulWidget {
   final Film _film;
 
   FilmPage(this._film);
 
-  Widget _rating() {
-    return Column(
-      children: [
-        Text(
-            "Rating",
-            style: TextStyle(fontSize: 20)
-        ),
-        Text(
-            "${_film.rating.rating}",
-            style: TextStyle(fontSize: 16)
-        )
-      ]
-    );
+  @override
+  _FilmPageState createState() => _FilmPageState();
+}
+
+class _FilmPageState extends State<FilmPage> {
+  @override
+  void initState() {
+    super.initState();
+    CinnemaApi.getFunctions(widget._film).then((functions) => print(functions));
   }
-  
+
+  Widget _rating() {
+    return Column(children: [
+      Text("Rating", style: TextStyle(fontSize: 20)),
+      Text("${widget._film.rating.rating}", style: TextStyle(fontSize: 16))
+    ]);
+  }
+
   Widget _genres() {
     String genres = "";
-    _film.genres.forEach((genre) => genres += "${genre.genre}, ");
-    return Column(
-        children: [
-          Text(
-              "Genres",
-              style: TextStyle(fontSize: 20)
-          ),
-          Text(
-              "${genres.substring(0, genres.length - 2)}",
-              style: TextStyle(fontSize: 16)
-          )
-        ]
-    );
+    widget._film.genres.forEach((genre) => genres += "${genre.genre}, ");
+    return Column(children: [
+      Text("Genres", style: TextStyle(fontSize: 20)),
+      Text("${genres.substring(0, genres.length - 2)}",
+          style: TextStyle(fontSize: 16))
+    ]);
   }
 
   Widget _languages() {
     String languages = "";
-    _film.languages.forEach((language) => languages += "${language.language}, ");
-    return Column(
-        children: [
-          Text(
-              "Languages",
-              style: TextStyle(fontSize: 20)
-          ),
-          Text(
-              "${languages.substring(0, languages.length - 2)}",
-              style: TextStyle(fontSize: 16)
-          )
-        ]
-    );
+    widget._film.languages
+        .forEach((language) => languages += "${language.language}, ");
+    return Column(children: [
+      Text("Languages", style: TextStyle(fontSize: 20)),
+      Text("${languages.substring(0, languages.length - 2)}",
+          style: TextStyle(fontSize: 16))
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Details")
-      ),
+      appBar: AppBar(title: Text("Details")),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Card(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
-                      child: Wrap(
-                          spacing: 16,
-                          direction: Axis.vertical,
-                          crossAxisAlignment: WrapCrossAlignment.center,
-                          runAlignment: WrapAlignment.center,
-                          children: [
-                            Text(
-                                _film.name,
-                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
-                            ),
-                            _rating(),
-                            _genres(),
-                            _languages()
-                          ]
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ]
-        ),
+        child: Column(children: [
+          Card(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+                    child: Wrap(
+                        spacing: 16,
+                        direction: Axis.vertical,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        runAlignment: WrapAlignment.center,
+                        children: [
+                          Text(widget._film.name,
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold)),
+                          _rating(),
+                          _genres(),
+                          _languages()
+                        ]),
+                  ),
+                )
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
